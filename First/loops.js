d51b00de6f90249e29157568dce9c8d1f535e8d6
krishna chaitnaya vuddagiri <vkchaitanya.535@gmail.com>
2022-01-28 09:26:18 +0530

    loops

---
+++ First/loops.js
@@ -0,0 +1,10 @@
+// Loops
+// Problem: Loops are used to repeat a block of code.
+// find the sum of all the numbers from 1 to 100
+// hint: use a loop
+// solution:
+var sum = 0;
+for (let i = 1; i <= 100; i++) {
+	sum += i;
+}
+console.log(sum);
