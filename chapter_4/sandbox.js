6afa0abf0a3456336bd18c4574f26d906fefa3f5
Krishna Chaitanya Vuddagiri <vkchaitanya.535@gmial.com>
2022-10-16 14:26:23 +0530

    callback and methods

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -13,3 +13,39 @@ const speak = function () {
 };
 
 speak();
+
+// arrow functions
+// const area = (radius) => {
+// 	return 3.14 * radius ** 2;
+// };
+
+const area = (radius) => 3.14 * radius ** 2;
+
+console.log(area(100));
+
+// callbacks and forearch
+console.log('==== CALLBACKS AND FOREACH ====');
+
+// callback
+const myfnuc = (callbackFnuc) => {
+	let value = 6;
+	callbackFnuc(value);
+};
+
+myfnuc((value) => console.log(value));
+
+let brothers = ['krishna', 'harsha', 'bharth', 'vamsi'];
+
+const logPerson = (person, index) => console.log(`${index + 1}. ${person}`);
+
+brothers.forEach((value, index) => logPerson(value, index));
+
+const ul = document.querySelector('.people');
+
+let html = ``;
+brothers.forEach(
+	(brother, index) => (html += `<li style="color: purple">${brother}</li>`)
+);
+
+console.log(html);
+ul.innerHTML = html;
